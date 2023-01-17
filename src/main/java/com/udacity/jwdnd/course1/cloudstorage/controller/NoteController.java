5ad707f25388c4180a48c23ad16f5a2e384578ce
Cris <cris@crisalb.com>
2023-01-17 11:32:14 -0800

    Note Controller

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/NoteController.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/NoteController.java
@@ -1,6 +1,9 @@
 package com.udacity.jwdnd.course1.cloudstorage.controller;
 
 import com.udacity.jwdnd.course1.cloudstorage.model.Note;
+import com.udacity.jwdnd.course1.cloudstorage.model.User;
+import com.udacity.jwdnd.course1.cloudstorage.services.CredentialService;
+import com.udacity.jwdnd.course1.cloudstorage.services.FileService;
 import com.udacity.jwdnd.course1.cloudstorage.services.NoteService;
 import com.udacity.jwdnd.course1.cloudstorage.services.UserService;
 import org.springframework.security.core.Authentication;
@@ -10,19 +13,66 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.io.IOException;
+
 @Controller
 @RequestMapping("/notes")
 public class NoteController {
   private UserService userService;
   private NoteService noteService;
-  public NoteController(UserService userService, NoteService noteService) {
+  private FileService fileService;
+  private CredentialService credentialService;
+
+  public NoteController(UserService userService, NoteService noteService, FileService fileService, CredentialService credentialService) {
     this.userService = userService;
     this.noteService = noteService;
-
+    this.fileService = fileService;
+    this.credentialService = credentialService;
   }
 
   @PostMapping("/note-upload")
-  public String createNote(Authentication authentication, @ModelAttribute Note note, Model model) {
-  return null;
+  public String createNote(Authentication authentication, @ModelAttribute Note note, Model model) throws IOException {
+    String noteError = null;
+    String noteUploadError = null;
+    User curr = userService.getUser(authentication.getName());
+    if(note.getNoteId() != null) {
+      try {
+        noteService.createNote(note);
+        model.addAttribute("noteUploadSuccess", "Note created");
+
+      } catch (Exception e){
+        noteError = e.toString();
+        model.addAttribute("noteError", noteError);
+      }
+    } else {
+      try {
+        noteService.updateNote(note);
+        model.addAttribute("updateSuccess", "Note Updated");
+
+      }catch (Exception e){
+        noteUploadError = e.toString();
+        model.addAttribute("uploadError", noteUploadError);
+      }
+    }
+    model.addAttribute("files", fileService.getFiles(curr));
+    model.addAttribute("notes", noteService.getNotes(curr));
+    model.addAttribute("credentials", credentialService.getAllCredentials(curr));
+    return "home";
+  }
+
+  public String deleteNote(Authentication authentication, @ModelAttribute Note note, Model model) throws IOException {
+    String deleteError = null;
+    User curr = userService.getUser(authentication.getName());
+    try {
+      noteService.deleteNote(note);
+      model.addAttribute("deleteSuccess", "Note Deleted");
+    } catch (Exception e) {
+      deleteError = e.toString();
+      model.addAttribute("deleteError", deleteError);
+    }
+    model.addAttribute("files", fileService.getFiles(curr));
+    model.addAttribute("notes", noteService.getNotes(curr));
+    model.addAttribute("credentials", credentialService.getAllCredentials(curr));
+    return "home";
   }
 }
